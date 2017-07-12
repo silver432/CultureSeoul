ad795455d5b789001713a8b4f92f22e62822a6d6
KyuJeong <heo0414@naver.com>
2017-07-13 00:31:12 +0900

    Community에 RecyclerVIew 추가 (숫자만 1~100)

--- app/src/main/java/com/example/kimjaeseung/cultureseoul2/community/CommunityFragment.java
+++ app/src/main/java/com/example/kimjaeseung/cultureseoul2/community/CommunityFragment.java
@@ -4,12 +4,15 @@ package com.example.kimjaeseung.cultureseoul2.community;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.kimjaeseung.cultureseoul2.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -18,6 +21,10 @@ import butterknife.ButterKnife;
 
 public class CommunityFragment extends Fragment {
     private final static String TAG = "CommunityFragment";
+    private static final int NUM_LIST_ITEMS = 100;
+    private CommunityAdapter mAdapter;
+
+    @Bind(R.id.community_list) RecyclerView mCommunityList;
 
     public CommunityFragment(){}
 
@@ -29,8 +36,19 @@ public class CommunityFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_community,container,false);
+
         ButterKnife.bind(this,view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mCommunityList.setLayoutManager(layoutManager);
+
+        mAdapter = new CommunityAdapter(NUM_LIST_ITEMS);
+        mCommunityList.setAdapter(mAdapter);
+
         return view;
     }
+
+
 }
