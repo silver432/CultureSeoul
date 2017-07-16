36db967863eb35c1f511a6cfaaaa24672816d801
Hwarang-Go <qubic98@gmail.com>
2017-07-16 19:14:23 +0900

    facebook login test

--- app/src/main/java/com/example/kimjaeseung/cultureseoul2/login/LoginActivity.java
+++ app/src/main/java/com/example/kimjaeseung/cultureseoul2/login/LoginActivity.java
@@ -5,20 +5,56 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+
 import com.example.kimjaeseung.cultureseoul2.R;
 import com.example.kimjaeseung.cultureseoul2.main.MainActivity;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+//facebook login
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class LoginActivity extends AppCompatActivity {
     private final static String TAG = "LoginActivity";
+
+    //facebook login
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+
+                    }
+                });
+
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
     }
 
     @OnClick({R.id.login_gotomain})
@@ -30,4 +66,8 @@ public class LoginActivity extends AppCompatActivity {
                 break;
         }
     }
+
+
+
+
 }
