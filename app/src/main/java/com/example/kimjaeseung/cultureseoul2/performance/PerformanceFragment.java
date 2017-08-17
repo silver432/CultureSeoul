0d1cc2f3d92b987b71bc468c4f539d18226b8437
silver432 <pub60@hanmail.net>
2017-08-17 15:05:13 +0900

    final

--- app/src/main/java/com/example/kimjaeseung/cultureseoul2/performance/PerformanceFragment.java
+++ app/src/main/java/com/example/kimjaeseung/cultureseoul2/performance/PerformanceFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
