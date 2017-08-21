f9dd5b62850e2e62653bdc9209645b0e09e918c3
silver432 <pub60@hanmail.net>
2017-08-21 18:20:36 +0900

    fix ChatRoom UI2

--- app/src/main/java/com/example/kimjaeseung/cultureseoul2/community/AddChatRoomActivity.java
+++ app/src/main/java/com/example/kimjaeseung/cultureseoul2/community/AddChatRoomActivity.java
@@ -1,5 +1,6 @@
 package com.example.kimjaeseung.cultureseoul2.community;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
@@ -10,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.NumberPicker;
