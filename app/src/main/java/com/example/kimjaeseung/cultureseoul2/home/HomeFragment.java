ef0c40f068734f2b9ffd3936d3ae7cf4158319f7
Nick0917 <nick0917@github.com>
2017-08-24 12:41:34 +0900

    홈 카테고리, 스플래시

--- app/src/main/java/com/example/kimjaeseung/cultureseoul2/home/HomeFragment.java
+++ app/src/main/java/com/example/kimjaeseung/cultureseoul2/home/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -15,6 +17,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.kimjaeseung.cultureseoul2.R;
 import com.example.kimjaeseung.cultureseoul2.domain.CultureEvent;
@@ -23,11 +27,14 @@ import com.example.kimjaeseung.cultureseoul2.login.LoginActivity;
 import com.example.kimjaeseung.cultureseoul2.network.CultureService;
 import com.example.kimjaeseung.cultureseoul2.performance.DetailActivity;
 import com.example.kimjaeseung.cultureseoul2.performance.PerformanceAdapter;
+import com.example.kimjaeseung.cultureseoul2.performance.PerformanceFragment;
+import com.example.kimjaeseung.cultureseoul2.performance.PerformanceGenreFragment;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.Transaction;
 
 import java.util.List;
 
@@ -44,22 +51,37 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by kimjaeseung on 2017. 7. 11..
  */
 
-public class HomeFragment extends Fragment implements HomeAdapter.HomeAdapterOnClickHandler{
+public class HomeFragment extends Fragment implements HomeAdapter.HomeAdapterOnClickHandler, View.OnClickListener {
     private final static String TAG = "HomeFragment";
     private String openApiKey = "74776b4f6873696c34364a6368704d";
 
     private FirebaseAuth mAuth;
     private GoogleApiClient mGoogleApiClient;
 
-    private static final int NUM_LIST_ITEMS = 100;
-    private HomeAdapter mAdapter;
-    @Bind(R.id.homeImageRecycler) RecyclerView mHomeList;
+
+    @Bind(R.id.category_concert) public TextView category_concert;
+    @Bind(R.id.category_classic) public TextView category_classic;
+    @Bind(R.id.category_musical) public TextView category_musical;
+    @Bind(R.id.category_theater) public TextView category_theater;
+    @Bind(R.id.category_dancing) public TextView category_dancing;
+    @Bind(R.id.category_exhibition) public TextView category_exhibition;
+    @Bind(R.id.category_traditional_music) public TextView category_traditional_music;
+    @Bind(R.id.category_festival) public TextView category_festival;
+    @Bind(R.id.category_solo) public TextView category_solo;
+    @Bind(R.id.category_movie) public TextView category_movie;
+    @Bind(R.id.category_cultureclass) public TextView category_cultureclass;
+
+    private String Genre;
+
 
     //@Bind(R.id.home_button_culturalevent) CulturalEventButtonTypeA culturalEventButtonTypeA;
-    @Bind(R.id.home_button_culturalevent) CulturalEventTypeMini culturalEventTypeMini;
-    public HomeFragment(){}
+    @Bind(R.id.home_button_culturalevent)
+    CulturalEventTypeMini culturalEventTypeMini;
 
-    public static Fragment getInstance(){
+    public HomeFragment() {
+    }
+
+    public static Fragment getInstance() {
         HomeFragment homeFragment = new HomeFragment();
         return homeFragment;
     }
@@ -67,78 +89,55 @@ public class HomeFragment extends Fragment implements HomeAdapter.HomeAdapterOnC
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
         setHasOptionsMenu(true);
         culturalEventTypeMini.setOpenAPIKey(openApiKey);
 
         //이후 예외처리
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        category_concert.setOnClickListener(this);
+        category_classic.setOnClickListener(this);
+        category_musical.setOnClickListener(this);
+        category_theater.setOnClickListener(this);
+        category_dancing.setOnClickListener(this);
+        category_exhibition.setOnClickListener(this);
+        category_traditional_music.setOnClickListener(this);
+        category_festival.setOnClickListener(this);
+        category_solo.setOnClickListener(this);
+        category_movie.setOnClickListener(this);
+        category_cultureclass.setOnClickListener(this);
 
         return view;
     }
 
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState)
-    {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        mHomeList.setLayoutManager(layoutManager);
-        mHomeList.setHasFixedSize(true);
-
-        mAdapter = new HomeAdapter(this.getContext(), this);
-        mHomeList.setAdapter(mAdapter);
-
-        loadData();
-
-    }
-
-    private void loadData()
-    {
-        // http://openapi.seoul.go.kr:8088/sample/json/SearchConcertDetailService/1/5/23075/
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://openapi.seoul.go.kr:8088")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        CultureService cultureService = retrofit.create(CultureService.class);
-        Call<CultureEventOutWrapper> callCultureEvent = cultureService.getCultureEvents(
-                "74776b4f6873696c34364a6368704d", "json", "SearchConcertDetailService", 1, 20, ""
-        );
-        callCultureEvent.enqueue(new Callback<CultureEventOutWrapper>()
-        {
-            @Override
-            public void onResponse(Call<CultureEventOutWrapper> call, Response<CultureEventOutWrapper> response)
-            {
-                if (response.isSuccessful())
-                {
-                    // 성공
-                    CultureEventOutWrapper result = response.body();
-                    List<CultureEvent> list = result.getCultureEventWrapper().getCultureEventList();
-                    mAdapter.setItemList(list); // recyclerview에 데이터 추가
-                    mAdapter.notifyAdapter();   // 화면 갱신
-                } else
-                {
-                    // 실패
-                }
-            }
-
-            @Override
-            public void onFailure(Call<CultureEventOutWrapper> call, Throwable t)
-            {
-                loadData();
-            }
-        });
     }
 
     @Override
-    public void onClick(CultureEvent cultureEvent)
-    {
+    public void onClick(CultureEvent cultureEvent) {
         Intent startToDetailActivity = new Intent(getActivity(), DetailActivity.class);
         startToDetailActivity.putExtra("key", cultureEvent);
         startActivity(startToDetailActivity);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String textId = ((TextView) v).getText().toString();
+        PerformanceGenreFragment.setmGenreTitle(textId);
+        switchFragment(PerformanceGenreFragment.getInstance());
+    }
+
+    private void switchFragment(Fragment fragment) {
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
     /*
